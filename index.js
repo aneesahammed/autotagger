77b98f2a9116516e7fe3e06f573cb5ef008ca098
Glitch (sable-possum) <none>
2019-04-11 14:49:31 +0000

    Committed by aneesahammed

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-module.exports = (app) => {
-  // Your code here
-  app.log('Yay! The app was loaded!')
+module.exports = app => {
+  app.log("Yay! The app was loaded!");
 
-  // example of probot responding 'Hello World' to a new issue being opened
-  app.on('issues.opened', async context => {
-    context.log(context.payload)
-    // `context` extracts information from the event, which can be passed to
-    // GitHub API calls. This will return:
-    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
-    const params = context.issue({body: 'Hello '+ context.payload.issue.user.login +'!'})
-    //const params = context.issue({body: JSON.stringify(context)})
-
-    // Post a comment on the issue
-    context.github.issues.createComment(params);
-    
+  app.on("issues.opened", async context => {
     let labelsToAdd = [];
-    labelsToAdd.push("awesome");
-    return context.github.issues.addLabels(context.issue({ labels: labelsToAdd }))
-  })
-}
+
+    let matches = context.payload.issue.title.match(/\[(.*?)\]/g);
+    matches.forEach(match => {
+      let tag = match.substring(
+        match.lastIndexOf("[") + 1,
+        match.lastIndexOf("]")
+      );
+      labelsToAdd.push({ name: tag, color: "005668" });
+    });
+
+    return context.github.issues.addLabels(
+      context.issue({ labels: labelsToAdd })
+    );
+  });
+};
